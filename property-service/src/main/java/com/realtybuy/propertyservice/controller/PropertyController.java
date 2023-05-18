358f35ba14c3fc13ec44c45865b4fdfd3d6aba1d
blad <bladimirbaezt@gmail.com>
2023-05-18 10:18:39 -0400

    Configured mongodb to allow adding and retrieving a property from the database.

--- property-service/src/main/java/com/realtybuy/propertyservice/controller/PropertyController.java
+++ property-service/src/main/java/com/realtybuy/propertyservice/controller/PropertyController.java
@@ -4,7 +4,6 @@ import com.realtybuy.propertyservice.dto.PropertyRequest;
 import com.realtybuy.propertyservice.dto.PropertyResponse;
 import com.realtybuy.propertyservice.service.PropertyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
